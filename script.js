042c6d428d01d8b0a620273634dfcb71debf9cb6
Gurusanthosh-P <146314247+Gurusanthosh-P@users.noreply.github.com>
2023-09-28 14:44:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let btn = document.getElementById("btn")
+let container = document.getElementById("container")
+const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+const audioArray = ['assets/Ak47.mp3','assets/pistol.mp3']
+
+
+btn.addEventListener('click',change)
+
+
+function change(){    
+    let frnd = '#'
+    for(i=1;i<=6;i++)
+    {
+        let color = hexRandom()
+        frnd+=color
+    }
+    let a = new Audio(playAudio())
+    a.play()
+    container.style.backgroundColor=frnd
+    btn.style.color=frnd
+    
+
+}
+
+function playAudio()
+{
+    let audioIndex = Math.floor(Math.random()*audioArray.length) 
+    return audioArray[audioIndex]
+
+}
+
+function hexRandom()
+{
+    let randomIndex = Math.floor(Math.random()*hex.length)
+    return hex[randomIndex]
+}
+
+
