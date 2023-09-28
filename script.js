e90aec961f386aa6e071c7ef43362835a8b6159d
Gurusanthosh-P <146314247+Gurusanthosh-P@users.noreply.github.com>
2023-09-28 14:45:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let btn = document.getElementById("btn")
 let container = document.getElementById("container")
 const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-const audioArray = ['assets/Ak47.mp3','assets/pistol.mp3']
+const audioArray = ['Ak47.mp3','pistol.mp3']
 
 
 btn.addEventListener('click',change)
